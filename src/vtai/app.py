42f97a699fbdbb18a610c215bafe49f4e19d4185
Vinh Nguyen <vinhnguyen2308@gmail.com>
2024-04-24 23:28:18 +0700

    Update app

--- src/vtai/app.py
+++ src/vtai/app.py
@@ -76,33 +76,6 @@ async def on_message(message: cl.Message) -> None:
         await __handle_conversation(message, messages)  # Process text messages
 
 
-async def __handle_conversation(
-    message: cl.Message, messages: List[Dict[str, str]]
-) -> None:
-    """
-    Handles text-based conversations with the user.
-    Routes the conversation based on settings and semantic understanding.
-    """
-    model = __get_settings(conf.SETTINGS_CHAT_MODEL)  # Get selected LLM model
-    msg = cl.Message(content="", author=model)  # Create initial response message
-    await msg.send()
-
-    query = message.content  # Get user query
-    # Add query to message history
-    __update_user_messages_history_with_context(query)
-
-    use_dynamic_conversation_routing = __get_settings(
-        conf.SETTINGS_USE_DYNAMIC_CONVERSATION_ROUTING
-    )
-
-    if use_dynamic_conversation_routing:
-        await __handle_dynamic_conversation_routing_chat(messages, model, msg, query)
-    else:
-        await __handle_trigger_async_chat(
-            llm_model=model, messages=messages, current_message=msg
-        )
-
-
 @cl.on_settings_update
 async def update_settings(settings: Dict[str, Any]) -> None:
     """
@@ -135,42 +108,47 @@ async def __handle_trigger_async_chat(
     """
     try:
         stream = await litellm.acompletion(
-            model=llm_model, messages=messages, stream=True, num_retries=2
+            model=llm_model,
+            messages=messages,
+            stream=True,
+            num_retries=2,
+            temperature=0.3,
         )
+
+        async for part in stream:
+            if token := part.choices[0].delta.content or "":
+                await current_message.stream_token(token)
+
+        content = current_message.content
+        __update_assistant_messages_history_with_context(content)
+
+        enable_tts_response = __get_settings(conf.SETTINGS_ENABLE_TTS_RESPONSE)
+        if enable_tts_response:
+            current_message.actions = [
+                cl.Action(
+                    name="speak_chat_response_action",
+                    value=content,
+                    label="Speak response",
+                )
+            ]
+
+        await current_message.update()
+
     except Exception as e:
-        await __handle_exception_error(llm_model, messages, current_message, e)
-        return
-
-    async for part in stream:
-        if token := part.choices[0].delta.content or "":
-            await current_message.stream_token(token)
-
-    content = current_message.content
-    __update_assistant_messages_history_with_context(content)
-
-    enable_tts_response = __get_settings(conf.SETTINGS_ENABLE_TTS_RESPONSE)
-    if enable_tts_response:
-        current_message.actions = [
-            cl.Action(
-                name="speak_chat_response_action", value=content, label="Speak response"
-            )
-        ]
-
-    await current_message.update()
+        await __handle_exception_error(e)
 
 
-async def __handle_exception_error(
-    llm_model: str,
-    messages: List[Dict[str, str]],
-    current_message: cl.Message,
-    e: Exception,
-) -> None:
+async def __handle_exception_error(e: Exception) -> None:
     """
     Handles exceptions that occur during LLM interactions.
     """
-    current_message.content = (
-        f"Something went wrong, please try again. Error type: {type(e)}, Error: {e}"
-    )
+
+    await cl.Message(
+        content=(
+            f"Something went wrong, please try again. Error type: {type(e)}, Error: {e}"
+        )
+    ).send()
+
     print(f"Error type: {type(e)}, Error: {e}")
 
 
@@ -250,9 +228,7 @@ async def __build_settings() -> Dict[str, Any]:
     return settings
 
 
-async def __handle_trigger_async_image_gen(
-    messages: List[Dict[str, str]], query: str
-) -> None:
+async def __handle_trigger_async_image_gen(query: str) -> None:
     """
     Triggers asynchronous image generation using the default image generation model.
     Sends the generated image and description to the user.
@@ -268,33 +244,33 @@ async def __handle_trigger_async_image_gen(
         image_response = await litellm.aimage_generation(
             prompt=query, model=image_gen_model
         )
+
+        image_gen_data = image_response["data"][0]
+        image_url = image_gen_data["url"]
+        revised_prompt = image_gen_data["revised_prompt"]
+
+        message = cl.Message(
+            author=image_gen_model,
+            content="Here's the image, along with a refined description based on your input:",
+            elements=[
+                cl.Image(url=image_url, name=query, display="inline"),
+                cl.Text(name="Description", content=revised_prompt, display="inline"),
+            ],
+            actions=[
+                cl.Action(
+                    name="speak_chat_response_action",
+                    value=revised_prompt,
+                    label="Speak response",
+                )
+            ],
+        )
+
+        __update_assistant_messages_history_with_context(revised_prompt)
+
+        await message.send()
+
     except Exception as e:
-        await __handle_exception_error(image_gen_model, messages, message, e)
-        return
-
-    image_gen_data = image_response["data"][0]
-    image_url = image_gen_data["url"]
-    revised_prompt = image_gen_data["revised_prompt"]
-
-    message = cl.Message(
-        author=image_gen_model,
-        content="Here's the image, along with a refined description based on your input:",
-        elements=[
-            cl.Image(url=image_url, name=query, display="inline"),
-            cl.Text(name="Description", content=revised_prompt, display="inline"),
-        ],
-        actions=[
-            cl.Action(
-                name="speak_chat_response_action",
-                value=revised_prompt,
-                label="Speak response",
-            )
-        ],
-    )
-
-    __update_assistant_messages_history_with_context(revised_prompt)
-
-    await message.send()
+        await __handle_exception_error(e)
 
 
 async def __handle_files_attachment(
@@ -320,7 +296,7 @@ async def __handle_files_attachment(
 
         elif "text" in mime_type:
             p = pathlib.Path(path, encoding="utf-8")
-            s = p.read_text()
+            s = p.read_text(encoding="utf-8")
             message.content = s
             await __handle_conversation(message, messages)
 
@@ -376,7 +352,7 @@ async def __handle_dynamic_conversation_routing_chat(
             Running route_choice_name: {route_choice_name}.
             Processing image generation..."""
         )
-        await __handle_trigger_async_image_gen(messages, query)
+        await __handle_trigger_async_image_gen(query)
 
     elif route_choice_name == SemanticRouterType.VISION_IMAGE_PROCESSING:
         urls = extract_url(query)
@@ -388,9 +364,7 @@ async def __handle_dynamic_conversation_routing_chat(
                 Processing with Vision model..."""
             )
             url = urls[0]
-            await __handle_vision(
-                input_image=url, prompt=query, messages=messages, is_local=False
-            )
+            await __handle_vision(input_image=url, prompt=query, is_local=False)
         else:
             print(
                 f"""💡
@@ -487,7 +461,11 @@ def __get_settings(key: str) -> Any:
     """
     Retrieves a specific setting value from the user session.
     """
-    return cl.user_session.get("chat_settings")[key]
+    settings = cl.user_session.get("chat_settings")
+    if settings is None:
+        return
+
+    return settings[key]
 
 
 @cl.action_callback("speak_chat_response_action")
@@ -546,3 +524,30 @@ def __update_messages_history_with_context(context: str, role: str):
 
     messages = cl.user_session.get("message_history") or []
     messages.append({"role": role, "content": context})
+
+
+async def __handle_conversation(
+    message: cl.Message, messages: List[Dict[str, str]]
+) -> None:
+    """
+    Handles text-based conversations with the user.
+    Routes the conversation based on settings and semantic understanding.
+    """
+    model = __get_settings(conf.SETTINGS_CHAT_MODEL)  # Get selected LLM model
+    msg = cl.Message(content="", author=model)  # Create initial response message
+    await msg.send()
+
+    query = message.content  # Get user query
+    # Add query to message history
+    __update_user_messages_history_with_context(query)
+
+    use_dynamic_conversation_routing = __get_settings(
+        conf.SETTINGS_USE_DYNAMIC_CONVERSATION_ROUTING
+    )
+
+    if use_dynamic_conversation_routing:
+        await __handle_dynamic_conversation_routing_chat(messages, model, msg, query)
+    else:
+        await __handle_trigger_async_chat(
+            llm_model=model, messages=messages, current_message=msg
+        )
