5ee30bd25cd25617f34d49f3808b58f6e5ab20a5
Vinh Nguyen <vinhnguyen2308@gmail.com>
2024-05-09 21:56:12 +0700

    Rename private methods

--- src/app.py
+++ src/app.py
@@ -98,9 +98,9 @@ async def start_chat():
     settings = await build_settings()
 
     # set selected LLM model for current settion's model
-    await __config_chat_session__(settings)
+    await _config_chat_session(settings)
 
-    if __is_currently_in_assistant_profile():
+    if _is_currently_in_assistant_profile():
         thread = await async_openai_client.beta.threads.create()
         cl.user_session.set("thread", thread)
 
@@ -156,7 +156,7 @@ async def run(thread_id: str, human_query: str, file_ids: List[str] = []):
                         thread_id=thread_id,
                     )
                 )
-                await __process_thread_message__(message_references, thread_message)
+                await _process_thread_message(message_references, thread_message)
 
             if step_details.type == "tool_calls":
                 for tool_call in step_details.tool_calls:
@@ -164,7 +164,7 @@ async def run(thread_id: str, human_query: str, file_ids: List[str] = []):
                         tool_call = DictToObject(tool_call)
 
                     if tool_call.type == "code_interpreter":
-                        await __process_tool_call__(
+                        await _process_tool_call(
                             step_references=step_references,
                             step=step,
                             tool_call=tool_call,
@@ -183,7 +183,7 @@ async def run(thread_id: str, human_query: str, file_ids: List[str] = []):
                         )
 
                     elif tool_call.type == "retrieval":
-                        await __process_tool_call__(
+                        await _process_tool_call(
                             step_references=step_references,
                             step=step,
                             tool_call=tool_call,
@@ -200,7 +200,7 @@ async def run(thread_id: str, human_query: str, file_ids: List[str] = []):
                             **json.loads(tool_call.function.arguments)
                         )
 
-                        await __process_tool_call__(
+                        await _process_tool_call(
                             step_references=step_references,
                             step=step,
                             tool_call=tool_call,
@@ -235,9 +235,9 @@ async def on_message(message: cl.Message) -> None:
     Processes text messages, file attachment, and routes conversations accordingly.
     """
 
-    if __is_currently_in_assistant_profile():
+    if _is_currently_in_assistant_profile():
         thread = cl.user_session.get("thread")  # type: Thread
-        files_ids = await __process_files__(message.elements)
+        files_ids = await _process_files(message.elements)
         await run(thread_id=thread.id, human_query=message.content, file_ids=files_ids)
 
     else:
@@ -245,11 +245,11 @@ async def on_message(message: cl.Message) -> None:
         messages = cl.user_session.get("message_history") or []  # Get message history
 
         if len(message.elements) > 0:
-            await __handle_files_attachment__(
+            await _handle_files_attachment(
                 message, messages
             )  # Process file attachments
         else:
-            await __handle_conversation(message, messages)  # Process text messages
+            await _handle_conversation(message, messages)  # Process text messages
 
 
 @cl.on_settings_update
@@ -297,22 +297,22 @@ async def on_speak_chat_response(action: cl.Action) -> None:
     """
     await action.remove()
     value = action.value
-    return await __handle_tts_response__(value)
+    return await _handle_tts_response(value)
 
 
-async def __handle_tts_response__(context: str) -> None:
+async def _handle_tts_response(context: str) -> None:
     """
     Generates and sends a TTS audio response using OpenAI's Audio API.
     """
-    enable_tts_response = __get_settings__(conf.SETTINGS_ENABLE_TTS_RESPONSE)
+    enable_tts_response = _get_settings(conf.SETTINGS_ENABLE_TTS_RESPONSE)
     if enable_tts_response is False:
         return
 
     if len(context) == 0:
         return
 
-    model = __get_settings__(conf.SETTINGS_TTS_MODEL)
-    voice = __get_settings__(conf.SETTINGS_TTS_VOICE_PRESET_MODEL)
+    model = _get_settings(conf.SETTINGS_TTS_MODEL)
+    voice = _get_settings(conf.SETTINGS_TTS_VOICE_PRESET_MODEL)
 
     with openai_client.audio.speech.with_streaming_response.create(
         model=model, voice=voice, input=context
@@ -333,18 +333,18 @@ async def __handle_tts_response__(context: str) -> None:
             ],
         ).send()
 
-        __update_msg_history_from_assistant_with_ctx__(context)
+        _update_msg_history_from_assistant_with_ctx(context)
 
 
-def __update_msg_history_from_user_with_ctx__(context: str):
-    __update_msg_history_with_ctx__(context=context, role="user")
+def _update_msg_history_from_user_with_ctx(context: str):
+    _update_msg_history_with_ctx(context=context, role="user")
 
 
-def __update_msg_history_from_assistant_with_ctx__(context: str):
-    __update_msg_history_with_ctx__(context=context, role="assistant")
+def _update_msg_history_from_assistant_with_ctx(context: str):
+    _update_msg_history_with_ctx(context=context, role="assistant")
 
 
-def __update_msg_history_with_ctx__(context: str, role: str):
+def _update_msg_history_with_ctx(context: str, role: str):
     if len(role) == 0 or len(context) == 0:
         return
 
@@ -352,47 +352,45 @@ def __update_msg_history_with_ctx__(context: str, role: str):
     messages.append({"role": role, "content": context})
 
 
-async def __handle_conversation(
+async def _handle_conversation(
     message: cl.Message, messages: List[Dict[str, str]]
 ) -> None:
     """
     Handles text-based conversations with the user.
     Routes the conversation based on settings and semantic understanding.
     """
-    model = __get_settings__(conf.SETTINGS_CHAT_MODEL)  # Get selected LLM model
+    model = _get_settings(conf.SETTINGS_CHAT_MODEL)  # Get selected LLM model
     msg = cl.Message(content="", author=APP_NAME)
     await msg.send()
 
     query = message.content  # Get user query
     # Add query to message history
-    __update_msg_history_from_user_with_ctx__(query)
+    _update_msg_history_from_user_with_ctx(query)
 
-    if __is_currently_in_assistant_profile():
+    if _is_currently_in_assistant_profile():
         mino = MinoAssistant(openai_client=async_openai_client)
         msg = cl.Message(content="", author=mino.name)
         await msg.send()
         await mino.run_assistant()
 
     else:
-        use_dynamic_conversation_routing = __get_settings__(
+        use_dynamic_conversation_routing = _get_settings(
             conf.SETTINGS_USE_DYNAMIC_CONVERSATION_ROUTING
         )
 
         if use_dynamic_conversation_routing:
-            await __handle_dynamic_conversation_routing_chat__(
-                messages, model, msg, query
-            )
+            await _handle_dynamic_conversation_routing_chat(messages, model, msg, query)
         else:
-            await __handle_trigger_async_chat(
+            await _handle_trigger_async_chat(
                 llm_model=model, messages=messages, current_message=msg
             )
 
 
-def __get_user_session_id__() -> str:
+def _get_user_session_id() -> str:
     return cl.user_session.get("id") or ""
 
 
-def __get_settings__(key: str) -> Any:
+def _get_settings(key: str) -> Any:
     """
     Retrieves a specific setting value from the user session.
     """
@@ -403,7 +401,7 @@ def __get_settings__(key: str) -> Any:
     return settings[key]
 
 
-async def __handle_vision__(
+async def _handle_vision(
     input_image: str,
     prompt: str,
     is_local: bool = False,
@@ -415,7 +413,7 @@ async def __handle_vision__(
     vision_model = (
         conf.DEFAULT_VISION_MODEL
         if is_local
-        else __get_settings__(conf.SETTINGS_VISION_MODEL)
+        else _get_settings(conf.SETTINGS_VISION_MODEL)
     )
 
     supports_vision = litellm.supports_vision(model=vision_model)
@@ -441,7 +439,7 @@ async def __handle_vision__(
 
     await message.send()
     vresponse = await litellm.acompletion(
-        user=__get_user_session_id__(),
+        user=__get_user_session_id(),
         model=vision_model,
         messages=[
             {
@@ -477,12 +475,12 @@ async def __handle_vision__(
         ],
     )
 
-    __update_msg_history_from_assistant_with_ctx__(description)
+    _update_msg_history_from_assistant_with_ctx(description)
 
     await message.send()
 
 
-async def __handle_trigger_async_chat(
+async def _handle_trigger_async_chat(
     llm_model: str, messages: List[Dict[str, str]], current_message: cl.Message
 ) -> None:
     """
@@ -490,8 +488,8 @@ async def __handle_trigger_async_chat(
     Streams the response back to the user and updates the message history.
     """
 
-    temperature = __get_settings__(conf.SETTINGS_TEMPERATURE)
-    top_p = __get_settings__(conf.SETTINGS_TOP_P)
+    temperature = _get_settings(conf.SETTINGS_TEMPERATURE)
+    top_p = _get_settings(conf.SETTINGS_TOP_P)
     try:
         stream = await litellm.acompletion(
             model=llm_model,
@@ -507,9 +505,9 @@ async def __handle_trigger_async_chat(
                 await current_message.stream_token(token)
 
         content = current_message.content
-        __update_msg_history_from_assistant_with_ctx__(content)
+        _update_msg_history_from_assistant_with_ctx(content)
 
-        enable_tts_response = __get_settings__(conf.SETTINGS_ENABLE_TTS_RESPONSE)
+        enable_tts_response = _get_settings(conf.SETTINGS_ENABLE_TTS_RESPONSE)
         if enable_tts_response:
             current_message.actions = [
                 cl.Action(
@@ -522,10 +520,10 @@ async def __handle_trigger_async_chat(
         await current_message.update()
 
     except Exception as e:
-        await __handle_exception_error(e)
+        await _handle_exception_error(e)
 
 
-async def __handle_exception_error(e: Exception) -> None:
+async def _handle_exception_error(e: Exception) -> None:
     """
     Handles exceptions that occur during LLM interactions.
     """
@@ -539,7 +537,7 @@ async def __handle_exception_error(e: Exception) -> None:
     print(f"Error type: {type(e)}, Error: {e}")
 
 
-async def __config_chat_session__(settings: Dict[str, Any]) -> None:
+async def _config_chat_session(settings: Dict[str, Any]) -> None:
     """
     Configures the chat session based on user settings and sets the initial system message.
     """
@@ -569,13 +567,13 @@ async def __config_chat_session__(settings: Dict[str, Any]) -> None:
         await cl.Message(content=msg).send()
 
 
-async def __handle_trigger_async_image_gen__(query: str) -> None:
+async def _handle_trigger_async_image_gen(query: str) -> None:
     """
     Triggers asynchronous image generation using the default image generation model.
     Sends the generated image and description to the user.
     """
     image_gen_model = conf.DEFAULT_IMAGE_GEN_MODEL
-    __update_msg_history_from_user_with_ctx__(query)
+    _update_msg_history_from_user_with_ctx(query)
 
     message = cl.Message(
         content="Sure! I'll create an image based on your description. This might take a moment, please be patient.",
@@ -583,11 +581,11 @@ async def __handle_trigger_async_image_gen__(query: str) -> None:
     )
     await message.send()
 
-    style = __get_settings__(conf.SETTINGS_IMAGE_GEN_IMAGE_STYLE)
-    quality = __get_settings__(conf.SETTINGS_IMAGE_GEN_IMAGE_QUALITY)
+    style = _get_settings(conf.SETTINGS_IMAGE_GEN_IMAGE_STYLE)
+    quality = _get_settings(conf.SETTINGS_IMAGE_GEN_IMAGE_QUALITY)
     try:
         image_response = await litellm.aimage_generation(
-            user=__get_user_session_id__(),
+            user=__get_user_session_id(),
             prompt=query,
             model=image_gen_model,
             style=style,
@@ -614,15 +612,15 @@ async def __handle_trigger_async_image_gen__(query: str) -> None:
             ],
         )
 
-        __update_msg_history_from_assistant_with_ctx__(revised_prompt)
+        _update_msg_history_from_assistant_with_ctx(revised_prompt)
 
         await message.send()
 
     except Exception as e:
-        await __handle_exception_error(e)
+        await _handle_exception_error(e)
 
 
-async def __handle_files_attachment__(
+async def _handle_files_attachment(
     message: cl.Message, messages: List[Dict[str, str]]
 ) -> None:
     """
@@ -640,20 +638,20 @@ async def __handle_files_attachment__(
         mime_type = file.mime or ""
 
         if "image" in mime_type:
-            await __handle_vision__(path, prompt=prompt, is_local=True)
+            await _handle_vision(path, prompt=prompt, is_local=True)
 
         elif "text" in mime_type:
             p = pathlib.Path(path, encoding="utf-8")
             s = p.read_text(encoding="utf-8")
             message.content = s
-            await __handle_conversation(message, messages)
+            await _handle_conversation(message, messages)
 
         elif "audio" in mime_type:
             f = pathlib.Path(path)
-            await __handle_audio_transcribe__(path, f)
+            await _handle_audio_transcribe(path, f)
 
 
-async def __handle_audio_transcribe__(path, audio_file):
+async def _handle_audio_transcribe(path, audio_file):
     model = conf.DEFAULT_WHISPER_MODEL
     transcription = await async_openai_client.audio.transcriptions.create(
         model=model, file=audio_file
@@ -669,11 +667,11 @@ async def __handle_audio_transcribe__(path, audio_file):
         ],
     ).send()
 
-    __update_msg_history_from_assistant_with_ctx__(text)
+    _update_msg_history_from_assistant_with_ctx(text)
     return text
 
 
-async def __handle_dynamic_conversation_routing_chat__(
+async def _handle_dynamic_conversation_routing_chat(
     messages: List[Dict[str, str]], model: str, msg: cl.Message, query: str
 ) -> None:
     """
@@ -683,7 +681,7 @@ async def __handle_dynamic_conversation_routing_chat__(
     route_choice = route_layer(query)
     route_choice_name = route_choice.name
 
-    should_trimmed_messages = __get_settings__(conf.SETTINGS_TRIMMED_MESSAGES)
+    should_trimmed_messages = _get_settings(conf.SETTINGS_TRIMMED_MESSAGES)
     if should_trimmed_messages:
         messages = trim_messages(messages, model)
 
@@ -700,7 +698,7 @@ async def __handle_dynamic_conversation_routing_chat__(
             Running route_choice_name: {route_choice_name}.
             Processing image generation..."""
         )
-        await __handle_trigger_async_image_gen__(query)
+        await _handle_trigger_async_image_gen(query)
 
     elif route_choice_name == SemanticRouterType.VISION_IMAGE_PROCESSING:
         urls = extract_url(query)
@@ -712,7 +710,7 @@ async def __handle_dynamic_conversation_routing_chat__(
                 Processing with Vision model..."""
             )
             url = urls[0]
-            await __handle_vision__(input_image=url, prompt=query, is_local=False)
+            await _handle_vision(input_image=url, prompt=query, is_local=False)
         else:
             print(
                 f"""💡
@@ -720,7 +718,7 @@ async def __handle_dynamic_conversation_routing_chat__(
                 Received no image urls/paths.
                 Processing with async chat..."""
             )
-            await __handle_trigger_async_chat(
+            await _handle_trigger_async_chat(
                 llm_model=model, messages=messages, current_message=msg
             )
     else:
@@ -729,18 +727,18 @@ async def __handle_dynamic_conversation_routing_chat__(
             Running route_choice_name: {route_choice_name}.
             Processing with async chat..."""
         )
-        await __handle_trigger_async_chat(
+        await _handle_trigger_async_chat(
             llm_model=model, messages=messages, current_message=msg
         )
 
 
-def __is_currently_in_assistant_profile() -> bool:
+def _is_currently_in_assistant_profile() -> bool:
     chat_profile = cl.user_session.get("chat_profile")
     return chat_profile == "Assistant"
 
 
 # Check if the files uploaded are allowed
-async def __check_files__(files: List[Element]):
+async def _check_files(files: List[Element]):
     for file in files:
         if file.mime not in allowed_mime:
             return False
@@ -748,7 +746,7 @@ async def __check_files__(files: List[Element]):
 
 
 # Upload files to the assistant
-async def __upload_files__(files: List[Element]):
+async def _upload_files(files: List[Element]):
     file_ids = []
     for file in files:
         uploaded_file = await async_openai_client.files.create(
@@ -758,23 +756,23 @@ async def __upload_files__(files: List[Element]):
     return file_ids
 
 
-async def __process_files__(files: List[Element]):
+async def _process_files(files: List[Element]):
     # Upload files if any and get file_ids
     file_ids = []
     if len(files) > 0:
-        files_ok = await __check_files__(files)
+        files_ok = await _check_files(files)
 
         if not files_ok:
             file_error_msg = f"Hey, it seems you have uploaded one or more files that we do not support currently, please upload only : {(',').join(allowed_mime)}"
             await cl.Message(content=file_error_msg).send()
             return file_ids
 
-        file_ids = await __upload_files__(files)
+        file_ids = await _upload_files(files)
 
     return file_ids
 
 
-async def __process_thread_message__(
+async def _process_thread_message(
     message_references: Dict[str, cl.Message], thread_message: Message
 ):
     for idx, content_message in enumerate(thread_message.content):
@@ -791,9 +789,7 @@ async def __process_thread_message__(
                 )
 
                 res_message = message_references[id].content
-                enable_tts_response = __get_settings__(
-                    conf.SETTINGS_ENABLE_TTS_RESPONSE
-                )
+                enable_tts_response = _get_settings(conf.SETTINGS_ENABLE_TTS_RESPONSE)
                 if enable_tts_response:
                     message_references[id].actions = [
                         cl.Action(
@@ -829,9 +825,7 @@ async def __process_thread_message__(
 
                 res_message = message_references[id].content
 
-                enable_tts_response = __get_settings__(
-                    conf.SETTINGS_ENABLE_TTS_RESPONSE
-                )
+                enable_tts_response = _get_settings(conf.SETTINGS_ENABLE_TTS_RESPONSE)
                 if enable_tts_response:
                     message_references[id].actions = [
                         cl.Action(
@@ -846,7 +840,7 @@ async def __process_thread_message__(
             print("unknown message type", type(content_message))
 
 
-async def __process_tool_call__(
+async def _process_tool_call(
     step_references: Dict[str, cl.Step],
     step: RunStep,
     tool_call: ToolCall,
