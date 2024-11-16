7079f08839c6d73a09478cee22c2ea36af6cd510
Vinh Nguyen <1097578+vinhnx@users.noreply.github.com>
2024-11-16 14:10:38 +0700

    Fix model profile builder

--- src/utils/llm_profile_builder.py
+++ src/utils/llm_profile_builder.py
@@ -3,6 +3,10 @@ from typing import Dict
 import chainlit as cl
 
 
-async def build_llm_profile(icons_provider_map: Dict[str, str]):
+def build_llm_profile(icons_provider_map: Dict[str, str]):
     for model_name, icon_path in icons_provider_map.items():
-        await cl.Avatar(name=model_name, path=icon_path).send()
+        return cl.ChatProfile(
+            name=model_name,
+            icon=icon_path,
+            markdown_description=f"The underlying LLM model is **{model_name}**.",
+        )
